938800f3d926ff39cc185fd0a90c6f4e85e2c6d0
Nic Ledoux <NL15025100@gmail.com>
2022-07-19 11:29:21 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+//Declare Variables
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+const PORT = 8500;
+
+//add model variable
+
+//Set middleware
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.DB_CONNECTION);
+
+app.listen(PORT, () => console.log(`Server is running on port {PORT}`));
